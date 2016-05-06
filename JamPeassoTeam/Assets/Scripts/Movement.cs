55369ef02a8565ccf366cd7c130f038e9c93b025
JoeFonzarelli <gsanseverino@enti.cat>
2016-05-07 01:13:48 +0200

    Preparing the Scene. Tapis Roulant's material has panner implemented. ALL WORKING

---
+++ JamPeassoTeam/Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+/*
+http://docs.unity3d.com/ScriptReference/TouchPhase.html
+http://docs.unity3d.com/ScriptReference/Input-touches.html
+http://docs.unity3d.com/ScriptReference/Input.GetTouch.html
+*/
